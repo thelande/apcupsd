a028de8ac5e4d962afc7db1620c9fac2522856f1
Thomas Helander <thomas.helander@gmail.com>
2024-02-28 12:55:06 -0800

    Really fix check for set but empty values

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ SETTINGS=(
 for s in "${SETTINGS[@]}"; do
     name="${ENVVAR_PREFIX}${s}"
     value="${!name}"
-    if [[ -n "${value+x}" ]]; then
+    if [[ -n "${!name+x}" ]]; then
         echo "Setting $s=$value"
         sed -i -r "s/^(#)?$s .*\$/$s $value/g" /etc/apcupsd/apcupsd.conf
     fi
